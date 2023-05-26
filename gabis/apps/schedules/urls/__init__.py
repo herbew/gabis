ac3e1fffb00b91959bd2d4d2004e4e2ad783384a
herbew <herbew@gmail.com>
2023-05-26 14:00:13 +0700

    add url guestbooks_urlpatterns

--- gabis/apps/schedules/urls/__init__.py
+++ gabis/apps/schedules/urls/__init__.py
@@ -2,7 +2,9 @@
 from __future__ import unicode_literals, absolute_import
 
 from gabis.apps.schedules.urls.bookings import urlpatterns as bookings_urlpatterns
+from gabis.apps.schedules.urls.guestbooks import urlpatterns as guestbooks_urlpatterns
 
 app_name = "schedules"
 
 urlpatterns = bookings_urlpatterns
+urlpatterns += guestbooks_urlpatterns
