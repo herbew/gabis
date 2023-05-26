ff53a4ab399081850d164a09a213d2abc0988084
herbew <herbew@gmail.com>
2023-05-26 14:54:04 +0700

    title event

--- gabis/apps/schedules/views/bookings.py
+++ gabis/apps/schedules/views/bookings.py
@@ -130,6 +130,7 @@ class TimeEventZiarahListView(ListView):
                     params_filter=params_filter,
                     page=page,
                     process=self.process, 
+                    event_title=self.event_filter
                    ) 
                 )
     
