22c78b2615d2b3bbdd15d61ed5e2cb7a85a9e52b
herbew <herbew@gmail.com>
2023-05-28 01:52:47 +0700

    change to import urls

--- gabis/apps/staffs/urls/__init__.py
+++ gabis/apps/staffs/urls/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
 
-from gabis.apps.staffs.schedules.urls.guestbooks import urlpatterns as guestbooks_urlpatterns
+from gabis.apps.staffs.urls.schedules.guestbooks import urlpatterns as guestbooks_urlpatterns
 
 app_name = "staffs"
 
