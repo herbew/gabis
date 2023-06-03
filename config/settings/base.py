a9fad96997a11569e5a5f70facea4033c2215e65
herbew <herbew@gmail.com>
2023-06-03 12:09:04 +0700

    change register to book

--- config/settings/base.py
+++ config/settings/base.py
@@ -395,7 +395,7 @@ QR_CODE_URL_PROTECTION = {
     constants.TOKEN_LENGTH: 30,                         # Optional random token length for URL protection. Defaults to 20.
     constants.SIGNING_KEY: 'my-secret-signing-key',     # Optional signing key for URL token. Uses SECRET_KEY if not defined.
     constants.SIGNING_SALT: 'my-signing-salt',          # Optional signing salt for URL token.
-    constants.ALLOWS_EXTERNAL_REQUESTS_FOR_REGISTERED_USER: True,  # Tells whether a registered user can request the QR code URLs from outside a site that uses this app. It can be a boolean value used for any user or a callable that takes a user as parameter. Defaults to False (nobody can access the URL without the signature token).
+    constants.ALLOWS_EXTERNAL_REQUESTS_FOR_BOOKED_USER: True,  # Tells whether a registered user can request the QR code URLs from outside a site that uses this app. It can be a boolean value used for any user or a callable that takes a user as parameter. Defaults to False (nobody can access the URL without the signature token).
     
 }
 
