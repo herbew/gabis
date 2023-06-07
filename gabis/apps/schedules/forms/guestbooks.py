c8c60988b2043b0ff6a033c0197904d2345358cc
herbew <herbew@gmail.com>
2023-06-07 13:02:18 +0700

    attended forms

--- gabis/apps/schedules/forms/guestbooks.py
+++ gabis/apps/schedules/forms/guestbooks.py
@@ -226,6 +226,8 @@ class GuestBookEventZiarahFilterForm(forms.Form):
         
         self.fields["params"].help_text = _("Input Your NIK/NIS/HP Number!")
         
+        self.fields['attended'].initial  = False
+        
 
 class GuestBookEventSeminarFilterForm(forms.Form):
     
