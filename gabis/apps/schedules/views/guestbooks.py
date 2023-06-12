7c23127fe7c4a3c4ed377cde9516d3ba2c335078
herbew <herbew@gmail.com>
2023-06-12 16:58:30 +0700

    change order_by

--- gabis/apps/schedules/views/guestbooks.py
+++ gabis/apps/schedules/views/guestbooks.py
@@ -792,7 +792,7 @@ class GuestBookSeminarPrintView(View):
         dtime_str = datetime.strftime(datetime.now(), "%d%m%Y_%H%M%S")
         
         for gb in self.model.objects.filter(time_event__event__name=SEMINAR_EVENT,
-            time_event__event__active=True).oreder_by(
+            time_event__event__active=True).order_by(
             "time_event__ordered","paid","attend","name"
             ):
             gb_list.append(dict(
@@ -837,7 +837,7 @@ class GuestBookZiarahPrintView(View):
         dtime_str = datetime.strftime(datetime.now(), "%d%m%Y_%H%M%S")
         
         for gb in self.model.objects.filter(time_event__event__name=ZIARAH_EVENT,
-            time_event__event__active=True).oreder_by(
+            time_event__event__active=True).order_by(
             "time_event__ordered","attend","name"
             ):
             gb_list.append(dict(
